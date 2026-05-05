c041540f94ae38fd20c39f40daf42be12c97e150
stefan judis <stefanjudis@gmail.com>
2026-05-05 20:52:24 +0200

    Generate broken trace

--- tests/02-shop.spec.ts
+++ tests/02-shop.spec.ts
@@ -15,8 +15,23 @@ test("[broken] completes a full shopping journey from search to cart", async ({
   await expect(page.getByTestId("login-name")).toBeVisible();
 
   await page.getByPlaceholder("Search for products...").fill("snowboard");
-  await page.getByRole('button', { name: 'Search' }).click()
+  await page.getByRole("button", { name: "Search" }).click();
   await expect(page.getByTestId("search-grid")).toBeVisible();
   await page.getByRole("link", { name: "Price: Low to high" }).click();
   await expect(page).toHaveURL(/sort=price-asc/);
 });
+
+/**
+test("what's broken here?", async ({ page }) => {
+  await page.goto("https://www.playwright-workshop.online/");
+  await page.getByRole("link", { name: "The Multi-managed Snowboard" }).click();
+  await page.getByRole("button", { name: "Add item to cart" }).click();
+
+  const cart = page.getByTestId("cart");
+  await cart
+    .getByRole("button", { name: "Increase item quantity" })
+    .first()
+    .dblclick();
+  await expect(cart.getByTestId("cart-item-quantity")).toHaveText("3");
+});
+*/

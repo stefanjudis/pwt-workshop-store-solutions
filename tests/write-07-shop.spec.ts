06d454e1653f21675e71acead1d33cb15e9a33fa
stefan judis <stefanjudis@gmail.com>
2026-05-05 23:58:48 +0200

    Rove write-07

---
+++ tests/write-07-shop.spec.ts
@@ -0,0 +1,43 @@
+import { expect, test } from "@playwright/test";
+
+test("take a screenshot", async ({ page, browserName }) => {
+  await page.goto("/");
+
+  // take a page screenshot
+  await page.screenshot({ path: "./home.png" });
+
+  // take a single screenshot
+  // await page.getByRole("link", { name: "Go to home" }).screenshot({
+  //   path: `./screenshots/${browserName}products-link.png`,
+  // });
+
+  // turn on visual regression testing
+  await expect(page.getByRole("link", { name: "Go to home" })).toHaveScreenshot(
+    "home-link.png",
+  );
+});
+
+test("masked product card screenshot", async ({ page }, testInfo) => {
+  await page.goto("/search");
+
+  await expect(page).toHaveScreenshot({
+    mask: [page.getByTestId("search-grid").getByRole("link")],
+  });
+});
+
+// ---------------- INLINE EXERCISE
+
+test("product card screenshot", async ({ page }, testInfo) => {
+  await page.goto("/search");
+
+  const screenshot = await page
+    .getByTestId("search-grid")
+    .getByRole("link")
+    .first()
+    .screenshot();
+
+  await testInfo.attach("screenshot", {
+    body: screenshot,
+    contentType: "image/png",
+  });
+});

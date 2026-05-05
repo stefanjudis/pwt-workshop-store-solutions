6b25e2575883ea3034253ce38145c36b05aa4c86
stefan judis <stefanjudis@gmail.com>
2026-05-05 20:40:53 +0200

    Add passing test

---
+++ tests/02-shop.spec.ts
@@ -0,0 +1,22 @@
+import { expect, test } from "@playwright/test";
+
+test("[broken] completes a full shopping journey from search to cart", async ({
+  page,
+}) => {
+  await page.goto("https://www.playwright-workshop.online");
+
+  await page.getByRole("link", { name: "Login" }).click();
+  await page.getByLabel("Name").fill("stefan");
+  await page.getByLabel("Your password").fill("12345678");
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await page.waitForURL(/\//);
+
+  await expect(page.getByTestId("login-name")).toBeVisible();
+
+  await page.getByPlaceholder("Search for products...").fill("snowboard");
+  await page.getByRole('button', { name: 'Search' }).click()
+  await expect(page.getByTestId("search-grid")).toBeVisible();
+  await page.getByRole("link", { name: "Price: Low to high" }).click();
+  await expect(page).toHaveURL(/sort=price-asc/);
+});

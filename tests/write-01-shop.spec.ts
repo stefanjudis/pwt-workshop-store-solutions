5c5b7b62571a72960331edb8aa65f327fbf020f2
stefan judis <stefanjudis@gmail.com>
2026-05-05 21:40:34 +0200

    Add mini exercise

---
+++ tests/write-01-shop.spec.ts
@@ -0,0 +1,15 @@
+import { expect, test } from "@playwright/test";
+
+test("locator-chaining", async ({ page }) => {
+  await page.goto("/lessons/writing-tests/01-locators-and-actions/");
+  const container = page.getByTestId("locator-chaining");
+  const listContainer = container
+    .getByRole("listitem")
+    .filter({ hasText: "Product 2" });
+
+  await listContainer.getByRole("button", { name: "Add to cart" }).click();
+
+  await expect(
+    listContainer.getByRole("button", { name: "Product added" }),
+  ).toBeVisible();
+});

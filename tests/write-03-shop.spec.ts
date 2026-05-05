2f4e2bd4a1bde3400ba830d57d89b90b3703fa1a
stefan judis <stefanjudis@gmail.com>
2026-05-05 22:43:16 +0200

    write 03

--- tests/write-03-shop.spec.ts
+++ tests/write-03-shop.spec.ts
@@ -13,3 +13,12 @@ test("aria-snapshot", async ({ page }) => {
       - button "Undo"
     `);
 });
+
+test("aria-description", async ({ page }) => {
+  await page.goto("/lessons/writing-tests/03-accessibility-assertions/");
+  const container = page.getByTestId("accessible-description-exercise");
+  await container.getByRole("button", { name: "Validate" }).click();
+  await expect(
+    container.getByLabel("Email address"),
+  ).toHaveAccessibleDescription("Please enter your email address.");
+});

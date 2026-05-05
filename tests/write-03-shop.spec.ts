2c456b5d9897908eb79a548e6e6063a3f8d5ee8d
stefan judis <stefanjudis@gmail.com>
2026-05-05 22:59:15 +0200

    Finish write-04

--- tests/write-03-shop.spec.ts
+++ tests/write-03-shop.spec.ts
@@ -1,4 +1,63 @@
-import test, { expect } from "@playwright/test";
+import { expect, test } from "@playwright/test";
+
+test("ARIA check on search navigations", async ({ page, isMobile }) => {
+  if (isMobile) test.skip();
+
+  await page.goto("/search/");
+
+  await expect(page.getByRole("main")).toMatchAriaSnapshot(`
+    - navigation "Collections":
+      - heading "Collections" [level=3]
+      - list
+    - heading "Products" [level=1]
+    - list
+    - navigation "Sort by":
+      - heading "Sort by" [level=3]
+      - list:
+        - listitem:
+          - link "Relevance"
+        - listitem:
+          - link "Trending"
+        - listitem:
+          - link "Latest arrivals"
+        - listitem:
+          - 'link "Price: Low to high"'
+        - listitem:
+          - 'link "Price: High to low"'
+    `);
+});
+
+test("ARIA check on main navigation", async ({ page, isMobile }) => {
+  await page.goto("https://www.playwright-workshop.online/");
+
+  if (isMobile) {
+    await expect(page.getByLabel("Main")).toMatchAriaSnapshot(`
+    - navigation "Main":
+      - button "Open mobile menu"
+      - link "Go to home":
+        - /url: /
+        - img "pwt logo"
+      - button "Open cart"
+    `);
+  } else {
+    await expect(page.getByLabel("Main")).toMatchAriaSnapshot(`
+      - navigation "Main":
+        - link "Go to home"
+        - list:
+          - listitem:
+            - link "Products":
+              - /url: /search/
+          - listitem:
+            - link "Workshop":
+              - /url: /lessons/
+        - textbox "Search for products..."
+        - button "Search"
+        - link "Login":
+          - /url: /login/
+        - button "Open cart"
+     `);
+  }
+});
 
 // ---------------- INLINE EXERCISE
 
